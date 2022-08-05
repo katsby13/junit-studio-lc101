ae1ccc80307b3f235cda3dcf88530b6bb9fd5d00
Katlyn McDonald <kkmcoding@gmail.com>
2022-08-04 20:47:25 -0400

    Tests complete (class not fixed yet)

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import main.BalancedBrackets;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +14,59 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void evenOrderedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void missingClosingBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[][]["));
+    }
+
+    @Test
+    public void unorderedEvenBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][][]["));
+    }
+
+    @Test
+    public void lettersWithinEvenBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[fnre], [ghiero], [rei]"));
+    }
+
+    @Test
+    public void lettersSurroundingBalancedBracketsStillReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("fnekl[dsnkl], efwe[fds]"));
+    }
+
+    @Test
+    public void balancedBracketsEqualsTrue() {
+        assertEquals(true, (BalancedBrackets.hasBalancedBrackets("[]")));
+    }
+
+    @Test
+    public void unbalancedBracketsReturnsFalse() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("[[["));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void unclosedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[nidsofnid"));
+    }
+
+    @Test
+    public void singleBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(("[")));
+    }
+
+    @Test
+    public void unorderedBracketsWithLettersReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("nffke]fef["));
+    }
 
 }
